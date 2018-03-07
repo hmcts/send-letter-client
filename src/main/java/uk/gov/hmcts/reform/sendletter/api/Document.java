691443bf8003666ffb606730dad9db894dab280a
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-03-07 15:59:36 +0000

    ROC-3252 Add hashcode and equals in domains

--- src/main/java/uk/gov/hmcts/reform/sendletter/api/Document.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/api/Document.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.sendletter.api;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class Document {
 
@@ -11,5 +12,23 @@ public class Document {
     public Document(String template, Map<String, Object> values) {
         this.template = template;
         this.values = values;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Document document = (Document) obj;
+        return Objects.equals(template, document.template)
+                && Objects.equals(values, document.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(template, values);
     }
 }
