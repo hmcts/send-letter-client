76e15a3a5b6c5fbf765aa01a64d30ced9c154ce1
Tim Jacomb <tim.jacomb@hmcts.net>
2018-03-16 18:09:43 +0000

    Cleanup

--- src/main/java/uk/gov/hmcts/reform/sendletter/api/SendLetterApi.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/api/SendLetterApi.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.reform.sendletter.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.codec.Decoder;
+import feign.jackson.JacksonDecoder;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import uk.gov.hmcts.reform.sendletter.SendLetterConfiguration;
+import uk.gov.hmcts.reform.sendletter.CustomFeignErrorDecoder;
 
-@FeignClient(name = "send-letter-api", url = "${send-letter.url}", configuration = SendLetterConfiguration.class)
+@FeignClient(name = "send-letter-api", url = "${send-letter.url}", configuration = SendLetterApi.SendLetterConfiguration.class)
 public interface SendLetterApi {
 
     @PostMapping(
@@ -20,4 +24,15 @@ public interface SendLetterApi {
             @RequestBody Letter letter
     );
 
+    class SendLetterConfiguration {
+        @Bean
+        Decoder feignDecoder(ObjectMapper objectMapper) {
+            return new JacksonDecoder(objectMapper);
+        }
+
+        @Bean
+        public CustomFeignErrorDecoder customFeignErrorDecoder() {
+            return new CustomFeignErrorDecoder();
+        }
+    }
 }
--- src/main/java/uk/gov/hmcts/reform/sendletter/healthcheck/SendLetterHealthApi.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/healthcheck/SendLetterHealthApi.java
@@ -4,22 +4,20 @@ import feign.codec.Decoder;
 import feign.jackson.JacksonDecoder;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @FeignClient(name = "send-letter-health", url = "${send-letter.url}",
-        configuration = SendLetterHealthApi.SendLetterConfiguration.class)
+        configuration = SendLetterHealthApi.SendLetterHealthConfiguration.class)
 public interface SendLetterHealthApi {
 
     @GetMapping(value = "/health", headers = CONTENT_TYPE + "=" + APPLICATION_JSON_UTF8_VALUE)
     InternalHealth health();
 
-    class SendLetterConfiguration {
+    class SendLetterHealthConfiguration {
         @Bean
-        @Primary
         Decoder feignDecoder() {
             return new JacksonDecoder();
         }
