03d51ac1618a8a25d375942fa6787ea52ffd807c
donatas M <1420191+doncem@users.noreply.github.com>
2020-02-19 12:10:47 +0000

    Fix client build (#35)

--- src/integrationTest/java/uk/gov/hmcts/reform/sendletter/AutoConfigurationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sendletter/AutoConfigurationTest.java
@@ -2,19 +2,26 @@ package uk.gov.hmcts.reform.sendletter;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.sendletter.api.SendLetterApi;
 import uk.gov.hmcts.reform.sendletter.healthcheck.SendLetterHealthApi;
+import uk.gov.hmcts.reform.sendletter.healthcheck.SendLetterHealthIndicator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(properties = {
+@EnableAutoConfiguration
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+    classes = SendLetterAutoConfiguration.class,
+    properties = {
         "send-letter.url=localhost"
-})
-@SpringJUnitConfig
+    }
+)
 public class AutoConfigurationTest {
 
     @Autowired
@@ -23,13 +30,14 @@ public class AutoConfigurationTest {
     @DisplayName("Should have HealthApi configured")
     @Test
     public void haveHealthCheck() {
-        assertThat(context.containsBeanDefinition(SendLetterHealthApi.class.getCanonicalName())).isTrue();
+        assertThat(context.getBeanNamesForType(SendLetterHealthApi.class)).hasSize(1);
+        assertThat(context.getBeanNamesForType(SendLetterHealthIndicator.class)).hasSize(1);
     }
 
     @DisplayName("Should have Api configured")
     @Test
     public void haveApi() {
-        assertThat(context.containsBeanDefinition(SendLetterApi.class.getCanonicalName())).isTrue();
-        assertThat(context.containsBeanDefinition(SendLetterAutoConfiguration.class.getCanonicalName())).isTrue();
+        assertThat(context.getBeanNamesForType(SendLetterApi.class)).hasSize(1);
+        assertThat(context.getBeanNamesForType(SendLetterAutoConfiguration.class)).hasSize(1);
     }
 }
