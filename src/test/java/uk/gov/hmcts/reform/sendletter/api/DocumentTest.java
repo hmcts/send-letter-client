9eeb79833ea33331bf6a1d8fd831aab7984f6b2c
Somesh Acharya <somesh.acharya@hmcts.net>
2020-08-07 16:28:35 +0100

    DTSBP-38: Added unit test for Document and Letter

--- src/test/java/uk/gov/hmcts/reform/sendletter/api/DocumentTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/api/DocumentTest.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class DocumentTest {
 
