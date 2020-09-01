aa5552c050b658da0ccd8298733780cb27feb7bd
Somesh Acharya <somesh.acharya@hmcts.net>
2020-09-01 18:19:24 +0100

    Revert "DTSBP-18: deprecate the api which accepts templates and data (#42)"
    This reverts commit 022dd5b8

--- src/main/java/uk/gov/hmcts/reform/sendletter/api/SendLetterApi.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/api/SendLetterApi.java
@@ -17,6 +17,17 @@ import uk.gov.hmcts.reform.sendletter.api.model.v3.LetterV3;
 public interface SendLetterApi {
 
     @PostMapping(
+            path = "/letters",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+
+    SendLetterResponse sendLetter(
+            @RequestHeader(name = "ServiceAuthorization", required = false) String serviceAuthHeader,
+            @RequestBody Letter letter
+    );
+
+    @PostMapping(
         path = "/letters",
         consumes = "application/vnd.uk.gov.hmcts.letter-service.in.letter.v2+json",
         produces = MediaType.APPLICATION_JSON_VALUE
